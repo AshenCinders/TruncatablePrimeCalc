1c91c5b07fa349d929d868308b9fe52290a9d35d
AshenCinders <albin@edeby.net>
2024-07-11 15:38:07 +0200

    refactor: remove/flatten sequential module

--- main.ex
+++ main.ex
@@ -1,89 +1,87 @@
 import(ElixirMath)
 
-defmodule Sequetial do
-  defmodule LeftTrunc do
-    @moduledoc """
-    Get a list of all left-truncatable prime number up to a given number n,
-    using function list_up_to(n).
-    """
+defmodule LeftTrunc do
+  @moduledoc """
+  Get a list of all left-truncatable prime number up to a given number n,
+  using function list_up_to(n).
+  """
 
-    # Base case, finished.
-    defp iter_chain(queue, result, stop_after) when hd(queue) > stop_after do
-      Enum.sort(result)
-    end
+  # Base case, finished.
+  defp iter_chain(queue, result, stop_after) when hd(queue) > stop_after do
+    Enum.sort(result)
+  end
 
-    # Prime check.
-    defp iter_chain(queue, result, stop_after) do
-      cond do
-        is_prime(hd(queue)) -> found_prime(queue, result, stop_after)
-        true -> iter_chain(tl(queue), result, stop_after)
-      end
-    end
-
-    defp found_prime(queue, result, stop_after) do
-      current = hd(queue)
-      result = [current | result]
-
-      next_most_digit_base = 10 ** length(Integer.digits(current))
-
-      next_block =
-        for d <- 1..9 do
-          d * next_most_digit_base + current
-        end
-
-      queue = queue ++ next_block
-
-      iter_chain(tl(queue), result, stop_after)
-    end
-
-    def list_up_to(n) do
-      iter_chain([2, 3, 5, 7], [], n)
+  # Prime check.
+  defp iter_chain(queue, result, stop_after) do
+    cond do
+      is_prime(hd(queue)) -> found_prime(queue, result, stop_after)
+      true -> iter_chain(tl(queue), result, stop_after)
     end
   end
 
-  defmodule RightTrunc do
-    @moduledoc """
-    Get a list of all right-truncatable prime number up to a given number n,
-    using function list_up_to(n).
-    """
+  defp found_prime(queue, result, stop_after) do
+    current = hd(queue)
+    result = [current | result]
 
-    # Base case, finished.
-    defp iter_chain(queue, result, stop_after) when hd(queue) > stop_after do
-      Enum.sort(result)
-    end
+    next_most_digit_base = 10 ** length(Integer.digits(current))
 
-    # Prime check.
-    defp iter_chain(queue, result, stop_after) do
-      cond do
-        is_prime(hd(queue)) -> found_prime(queue, result, stop_after)
-        true -> iter_chain(tl(queue), result, stop_after)
+    next_block =
+      for d <- 1..9 do
+        d * next_most_digit_base + current
       end
+
+    queue = queue ++ next_block
+
+    iter_chain(tl(queue), result, stop_after)
+  end
+
+  def list_up_to(n) do
+    iter_chain([2, 3, 5, 7], [], n)
+  end
+end
+
+defmodule RightTrunc do
+  @moduledoc """
+  Get a list of all right-truncatable prime number up to a given number n,
+  using function list_up_to(n).
+  """
+
+  # Base case, finished.
+  defp iter_chain(queue, result, stop_after) when hd(queue) > stop_after do
+    Enum.sort(result)
+  end
+
+  # Prime check.
+  defp iter_chain(queue, result, stop_after) do
+    cond do
+      is_prime(hd(queue)) -> found_prime(queue, result, stop_after)
+      true -> iter_chain(tl(queue), result, stop_after)
     end
+  end
 
-    defp found_prime(queue, result, stop_after) do
-      current = hd(queue)
-      result = [current | result]
+  defp found_prime(queue, result, stop_after) do
+    current = hd(queue)
+    result = [current | result]
 
-      next_block =
-        for d <- [1, 3, 7, 9] do
-          current * 10 + d
-        end
+    next_block =
+      for d <- [1, 3, 7, 9] do
+        current * 10 + d
+      end
 
-      queue = queue ++ next_block
+    queue = queue ++ next_block
 
-      iter_chain(tl(queue), result, stop_after)
-    end
+    iter_chain(tl(queue), result, stop_after)
+  end
 
-    def list_up_to(n) do
-      iter_chain([2, 3, 5, 7], [], n)
-    end
+  def list_up_to(n) do
+    iter_chain([2, 3, 5, 7], [], n)
   end
 end
 
 # Example of left truncatable numbers.
-# Sequetial.LeftTrunc.list_up_to(1000)
+# LeftTrunc.list_up_to(1000)
 # |> IO.inspect(limit: :infinity)
 
 # Example of right truncatable numbers.
-# Sequetial.RightTrunc.list_up_to(1000)
+# RightTrunc.list_up_to(1000)
 # |> IO.inspect(limit: :infinity)
